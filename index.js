68d717f6ed278cbd8c1bac48135eae06ddc7c18a
mansijoshi17 <mansijoshi1777@gmail.com>
2023-12-02 16:56:40 +0530

    Created external api for send email post api

--- index.js
+++ index.js
@@ -1,76 +1,83 @@
-const { Requester, Validator } = require('@chainlink/external-adapter')
+const { Requester, Validator } = require("@chainlink/external-adapter");
 
 // Define custom error scenarios for the API.
 // Return true for the adapter to retry.
 const customError = (data) => {
-  if (data.Response === 'Error') return true
-  return false
-}
+  if (data.Response === "Error") return true;
+  return false;
+};
 
 // Define custom parameters to be used by the adapter.
 // Extra parameters can be stated in the extra object,
 // with a Boolean value indicating whether or not they
 // should be required.
 const customParams = {
-  base: ['base', 'from', 'coin'],
-  quote: ['quote', 'to', 'market'],
-  endpoint: false
-}
+  message: ["message"],
+  reply_to: ["reply_to"],
+  endpoint: false,
+};
 
 const createRequest = (input, callback) => {
   // The Validator helps you validate the Chainlink request data
-  const validator = new Validator(callback, input, customParams)
-  const jobRunID = validator.validated.id
-  const endpoint = validator.validated.data.endpoint || 'price'
-  const url = `https://min-api.cryptocompare.com/data/${endpoint}`
-  const fsym = validator.validated.data.base.toUpperCase()
-  const tsyms = validator.validated.data.quote.toUpperCase()
+  const validator = new Validator(callback, input, customParams);
+  const jobRunID = validator.validated.id;
+  const endpoint = validator.validated.data.endpoint || "send-email";
+  const url = `http://localhost:4000/wwl/${endpoint}`;
+  const message = validator.validated.data.message;
+  const reply_to = validator.validated.data.reply_to;
 
   const params = {
-    fsym,
-    tsyms
-  }
+    message,
+    reply_to,
+  };
 
   // This is where you would add method and headers
   // you can add method like GET or POST and add it to the config
   // The default is GET requests
-  // method = 'get' 
+  // method = 'get'
   // headers = 'headers.....'
   const config = {
+    method: "post",
     url,
-    params
-  }
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: params,
+  };
 
   // The Requester allows API calls be retry in case of timeout
   // or connection failure
   Requester.request(config, customError)
-    .then(response => {
+    .then((response) => {
       // It's common practice to store the desired value at the top-level
       // result key. This allows different adapters to be compatible with
       // one another.
-      response.data.result = Requester.validateResultNumber(response.data, [tsyms])
-      callback(response.status, Requester.success(jobRunID, response))
+      console.log(response.data, "response");
+      response.data.result = Requester.validateResultNumber(response.data, [
+        "success",
+      ]);
+      callback(response.status, Requester.success(jobRunID, response));
     })
-    .catch(error => {
-      callback(500, Requester.errored(jobRunID, error))
-    })
-}
+    .catch((error) => {
+      callback(500, Requester.errored(jobRunID, error));
+    });
+};
 
 // This is a wrapper to allow the function to work with
 // GCP Functions
 exports.gcpservice = (req, res) => {
   createRequest(req.body, (statusCode, data) => {
-    res.status(statusCode).send(data)
-  })
-}
+    res.status(statusCode).send(data);
+  });
+};
 
 // This is a wrapper to allow the function to work with
 // AWS Lambda
 exports.handler = (event, context, callback) => {
   createRequest(event, (statusCode, data) => {
-    callback(null, data)
-  })
-}
+    callback(null, data);
+  });
+};
 
 // This is a wrapper to allow the function to work with
 // newer AWS Lambda implementations
@@ -79,11 +86,11 @@ exports.handlerv2 = (event, context, callback) => {
     callback(null, {
       statusCode: statusCode,
       body: JSON.stringify(data),
-      isBase64Encoded: false
-    })
-  })
-}
+      isBase64Encoded: false,
+    });
+  });
+};
 
 // This allows the function to be exported for testing
 // or for running in express
-module.exports.createRequest = createRequest
+module.exports.createRequest = createRequest;
